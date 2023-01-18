0daae354fd5a8e107b96b33080a89653492df65f
BahadurAliShah <sayyedbahadur@gmail.com>
2023-01-18 22:19:35 +0500

    navigation and 404 page added

---
+++ src/Pages/notFound.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+
+export default function NotFound() {
+    return (
+        <>
+            {/*
+        This example requires updating your template:
+
+        ```
+        <html class="h-full">
+        <body class="h-full">
+        ```
+      */}
+            <div className="flex min-h-full flex-col bg-white pt-16 pb-12">
+                <main className="m-auto flex w-full max-w-7xl flex-grow flex-col justify-center px-6 lg:px-8 py-32">
+                    <div className="text-center">
+                        <p className="text-base font-semibold text-indigo-600">404</p>
+                        <h1 className="mt-2 text-4xl font-bold tracking-tight text-gray-900 sm:text-5xl">Page not
+                            found.</h1>
+                        <p className="mt-2 text-base text-gray-500">Sorry, we couldn’t find the page you’re looking
+                            for.</p>
+                        <div className="mt-6">
+                            <a href="/home" className="text-base font-medium text-indigo-600 hover:text-indigo-500">
+                                Go back home
+                                <span aria-hidden="true"> &rarr;</span>
+                            </a>
+                        </div>
+                    </div>
+                </main>
+            </div>
+        </>
+    )
+}
--- src/index.js
+++ src/index.js
@@ -1,14 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import {Routes, Route, BrowserRouter as Router} from 'react-router-dom';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+import Home from './Pages/home';
+import NotFound from './Pages/notFound';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <React.StrictMode>
+        <Router>
+            <Routes>
+                <Route path="/" element={<Home/>}/>
+                <Route path={"/home"} element={<Home/>}/>
+                <Route path="*" element={<NotFound/>}/>
+            </Routes>
+        </Router>
+    </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
