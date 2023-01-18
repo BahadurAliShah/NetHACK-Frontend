ff9479f77ac443cef5e8fc488321862c1fab19c3
BahadurAliShah <sayyedbahadur@gmail.com>
2023-01-19 00:10:19 +0500

    Sidebar Component Added

--- src/Pages/home.jsx
+++ src/Pages/home.jsx
@@ -1,9 +1,10 @@
 import React from "react";
+import Sidebar from "../Components/sidebar";
 
 export default function Home() {
     return (
-        <div>
-        <h1>Home</h1>
-        </div>
+        <>
+            <Sidebar />
+        </>
     );
 }
