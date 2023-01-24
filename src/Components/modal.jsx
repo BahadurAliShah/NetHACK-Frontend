453ee11bb9e1e55a148ff41e416a48875ab277bd
BahadurAliShah <sayyedbahadur@gmail.com>
2023-01-24 12:56:31 +0500

    Modal width set to 75%

--- src/Components/modal.jsx
+++ src/Components/modal.jsx
@@ -32,7 +32,7 @@ export default function Modal(props, {children}) {
                             leaveTo="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
                         >
                             <Dialog.Panel
-                                className="relative transform overflow-hidden rounded-lg bg-white px-4 pt-5 pb-4 text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-lg sm:p-6">
+                                className="relative transform overflow-hidden rounded-lg bg-white px-4 pt-5 pb-4 text-left shadow-xl transition-all sm:my-8 sm:w-full sm:w-9/12 sm:p-6">
                                 <div className="absolute top-0 right-0 hidden pt-4 pr-4 sm:block">
                                     <button
                                         type="button"
