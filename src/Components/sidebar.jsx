855c4f0b82d17dc040fb71c1cc930bc65d4bce8c
BahadurAliShah <sayyedbahadur@gmail.com>
2023-01-25 15:23:55 +0500

    Sidebar navigation source changed from constants.js to redux

--- src/Components/sidebar.jsx
+++ src/Components/sidebar.jsx
@@ -1,11 +1,16 @@
 import React from "react";
 import {Link} from "react-router-dom";
+import {useSelector, useDispatch} from "react-redux";
+import {selectMenuItemAction, selectSubMenuItemAction} from "../Store/Actions/sidebarMenuActions";
 
 function classNames(...classes) {
     return classes.filter(Boolean).join(' ')
 }
 
-export default function Sidebar(props) {
+export default function Sidebar() {
+    const navigation = useSelector(state => state.navigation);
+    const dispatch = useDispatch();
+
     return (
         <>
             <div>
@@ -21,34 +26,59 @@ export default function Sidebar(props) {
                         </div>
                         <div className="flex flex-1 flex-col overflow-y-auto">
                             <nav className="flex-1 space-y-1 px-2 py-4">
-                                {props.navigation.map((item) => (
-                                    <Link
-                                        key={item.name}
-                                        to={item.href}
-                                        onClick={(e) => {
-                                            const updatedNavigation = props.navigation.map(uItem => {
-                                                (uItem.name === item.name) ? uItem.current = true : uItem.current = false;
-                                                return uItem;
-                                            });
-                                            props.setNavigation([...updatedNavigation]);
-                                        }
-                                        }
-                                        className={classNames(
-                                            item.current
-                                                ? 'bg-gray-900 text-white'
-                                                : 'text-gray-300 hover:bg-gray-700 hover:text-white',
-                                            'group flex items-center px-2 py-2 text-sm font-medium rounded-md'
-                                        )}
-                                    >
-                                        <item.icon
+                                {navigation.map((item) => (
+                                    <>
+                                        <Link
+                                            key={item.name}
+                                            to={item.href}
+                                            onClick={(e) =>
+                                                dispatch(selectMenuItemAction(item))
+                                            }
                                             className={classNames(
-                                                item.current ? 'text-gray-300' : 'text-gray-400 group-hover:text-gray-300',
-                                                'mr-3 flex-shrink-0 h-6 w-6'
+                                                item.current
+                                                    ? 'bg-gray-900 text-white'
+                                                    : 'text-gray-300 hover:bg-gray-700 hover:text-white',
+                                                'group flex items-center px-2 py-2 text-sm font-medium rounded-md'
                                             )}
-                                            aria-hidden="true"
-                                        />
-                                        {item.name}
-                                    </Link>
+                                        >
+                                            <item.icon
+                                                className={classNames(
+                                                    item.current ? 'text-gray-300' : 'text-gray-400 group-hover:text-gray-300',
+                                                    'mr-3 flex-shrink-0 h-6 w-6'
+                                                )}
+                                                aria-hidden="true"
+                                            />
+                                            {item.name}
+                                        </Link>
+
+                                        {item.current && item.subNavigation && (
+                                            <div className="mt-1 space-y-1">
+                                                {item.subNavigation.map((subItem) => (
+                                                    <Link
+                                                        key={subItem.name}
+                                                        onClick={(e) =>
+                                                            dispatch(selectSubMenuItemAction(item, subItem))
+                                                        }
+                                                        className={classNames(
+                                                            subItem.current
+                                                                ? 'bg-gray-900 text-white'
+                                                                : 'text-gray-300 hover:bg-gray-700 hover:text-white',
+                                                            'group flex items-center ml-3 px-2 py-2 text-xs font-medium rounded-md'
+                                                        )}
+                                                    >
+                                                        <subItem.icon
+                                                            className={classNames(
+                                                                subItem.current ? 'text-gray-300' : 'text-gray-400 group-hover:text-gray-300',
+                                                                'mr-3 flex-shrink-0 h-4 w-4'
+                                                            )}
+                                                            aria-hidden="true"
+                                                        />
+                                                        {subItem.name}
+                                                    </Link>
+                                                ))}
+                                            </div>
+                                        )}
+                                    </>
                                 ))}
                             </nav>
                         </div>
