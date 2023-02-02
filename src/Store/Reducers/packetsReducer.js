3e731c22a00d2f57894d3e5f942eb08ed9f8e145
BahadurAliShah <siemless.sec@gmail.com>
2023-02-02 21:24:04 +0500

    Speed Page Completed

--- src/Store/Reducers/packetsReducer.js
+++ src/Store/Reducers/packetsReducer.js
@@ -15,6 +15,90 @@ const initialState = {
     averageSpeed: []
 }
 
+const addData = (data, newValue) => {
+    let nullArray = false;
+    for (let i = 0; i < data.length; i++) {
+        if (data[i].y === null) {
+            nullArray = i;
+        }
+    }
+    if (nullArray !== false) {
+        data[nullArray] = {...data[nullArray], y: newValue};
+    } else {
+        for (let i = 0; i < data.length; i++) {
+            if (i === data.length - 1) {
+                data[i].y = newValue;
+            } else {
+                data[i].y = data[i + 1].y;
+            }
+        }
+    }
+    return [...data];
+}
+
+const SampleData = [
+    {x: 0, y: null},
+    {x: 1, y: null},
+    {x: 2, y: null},
+    {x: 3, y: null},
+    {x: 4, y: null},
+    {x: 5, y: null},
+    {x: 6, y: null},
+    {x: 7, y: null},
+    {x: 8, y: null},
+    {x: 9, y: null},
+    {x: 10, y: null},
+    {x: 11, y: null},
+    {x: 12, y: null},
+    {x: 13, y: null},
+    {x: 14, y: null},
+    {x: 15, y: null},
+    {x: 16, y: null},
+    {x: 17, y: null},
+    {x: 18, y: null},
+    {x: 19, y: null},
+    {x: 20, y: null},
+    {x: 21, y: null},
+    {x: 22, y: null},
+    {x: 23, y: null},
+    {x: 24, y: null},
+    {x: 25, y: null},
+    {x: 26, y: null},
+    {x: 27, y: null},
+    {x: 28, y: null},
+    {x: 29, y: null},
+    {x: 30, y: null},
+    {x: 31, y: null},
+    {x: 32, y: null},
+    {x: 33, y: null},
+    {x: 34, y: null},
+    {x: 35, y: null},
+    {x: 36, y: null},
+    {x: 37, y: null},
+    {x: 38, y: null},
+    {x: 39, y: null},
+    {x: 40, y: null},
+    {x: 41, y: null},
+    {x: 42, y: null},
+    {x: 43, y: null},
+    {x: 44, y: null},
+    {x: 45, y: null},
+    {x: 46, y: null},
+    {x: 47, y: null},
+    {x: 48, y: null},
+    {x: 49, y: null},
+    {x: 50, y: null},
+    {x: 51, y: null},
+    {x: 52, y: null},
+    {x: 53, y: null},
+    {x: 54, y: null},
+    {x: 55, y: null},
+    {x: 56, y: null},
+    {x: 57, y: null},
+    {x: 58, y: null},
+    {x: 59, y: null},
+];
+
 export const packetsReducer = (state = initialState, action) => {
     switch (action.type) {
         case addPacket:
@@ -24,9 +108,29 @@ export const packetsReducer = (state = initialState, action) => {
         case setPacketsPage:
             return {...state, page: action.page};
         case setDevices:
-            return {...state, devices: action.devices};
+            return {
+                ...state, devices: action.devices.map(device => {
+                        return {
+                            ...device,
+                            selected: false
+                        }
+                    }
+                )
+            };
         case setInstantaneousSpeed:
-            return {...state, instantaneousSpeed: action.instantaneousSpeed};
+            const newInstantaneousSpeed = state.devices.map((device, index) => {
+                if (index >= state.instantaneousSpeed.length) {
+                    return {
+                        send: addData([...SampleData], action.instantaneousSpeed[index]['instantanouesSSpeed']),
+                        receive: addData([...SampleData], action.instantaneousSpeed[index]['instantanouesRSpeed'])
+                    }
+                }
+                return {
+                    send: addData([...state.instantaneousSpeed[index]['send']], action.instantaneousSpeed[index]['instantanouesSSpeed']),
+                    receive: addData([...state.instantaneousSpeed[index]['receive']], action.instantaneousSpeed[index]['instantanouesRSpeed'])
+                }
+            });
+            return {...state, instantaneousSpeed: newInstantaneousSpeed};
         case setAverageSpeed:
             return {...state, averageSpeed: action.averageSpeed};
         default:
